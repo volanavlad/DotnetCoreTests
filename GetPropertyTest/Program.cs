4b1f2b3d9b105f719d29b2dd9b5febca49422d60
Володько А В <Anatoly.V.Volodko@galaktika.by>
2019-09-11 17:29:11 +0300

    DotnetCoreTests GetPropertyTest with Cyrillic

---
+++ GetPropertyTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace GetPropertyTest
+{
+
+    class Program
+    {
+        public string Свойство_С_Киррилицей { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            var result = typeof(Program).GetProperty(nameof(Свойство_С_Киррилицей),
+                BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public);
+            Console.WriteLine(
+                "typeof(Program).GetProperty(nameof(Свойство_С_Киррилицей),BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public)==" +
+                result?.ToString());
+            result = typeof(Program).GetProperty(nameof(Свойство_С_Киррилицей),
+                BindingFlags.Instance | BindingFlags.Public);
+            Console.WriteLine(
+                "typeof(Program).GetProperty(nameof(Свойство_С_Киррилицей),BindingFlags.Instance | BindingFlags.Public)==" +
+                result);
+
+            var expr = Expression.New(typeof(Program).GetConstructor(new Type[0]));
+            var exprProp = Expression.Property(expr, nameof(Свойство_С_Киррилицей));
+
+            Console.WriteLine("Property expression was sucsessfully created.");
+        }
+    }
+}
